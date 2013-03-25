5abf674f4fec4550c0ad26000915607e5d002889
Adam Pritchard <pritchard.adam@gmail.com>
2013-03-25 10:11:21 -0400

    Added Social Share Privacy support

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,10 @@
   'use strict';
 
   $(function() {
+    $.fn.socialSharePrivacy.settings.order = ['twitter', 'gplus', 'hackernews', 'facebook', 'linkedin', 'stumbleupon'];
+    $.fn.socialSharePrivacy.settings.path_prefix = '/assets/js/socialshareprivacy/';
+
+    $('.share-panel').socialSharePrivacy();
 
     /*
     // Set the navbar active element to the current page
